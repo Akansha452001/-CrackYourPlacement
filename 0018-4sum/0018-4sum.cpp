95e769a7f9f69de7fb0280465f8b6268cd0b8ad0
Akansha452001 <149137458+Akansha452001@users.noreply.github.com>
2024-07-29 23:56:07 +0530

    Time: 28 ms (55.65%), Space: 16.2 MB (44.37%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,31 +7,31 @@ public:
 
     sort(nums.begin(), nums.end());
 
-    for (int i = 0; i < nums.size() - 3; i++) {
-        // Skip duplicates for i
-        if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-        for (int j = i + 1; j < nums.size() - 2; j++) {
-            // Skip duplicates for j
-            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-            int k = j + 1, l = nums.size() - 1;
-            while (k < l) {
-                long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
-                if (sum == target) {
-                    ans.push_back({nums[i], nums[j], nums[k], nums[l]});
-                    while (k < l && nums[k] == nums[k + 1]) k++; // Skip duplicates for k
-                    while (k < l && nums[l] == nums[l - 1]) l--; // Skip duplicates for l
-                    k++;
-                    l--;
-                } else if (sum < target) {
-                    k++;
-                } else {
-                    l--;
-                }
-            }
-        }
+   for(int i=0;i<=nums.size()-4;i++){
+    if(i>0 && nums[i-1]==nums[i])
+    continue;
+    for(int j=i+1;j<=nums.size()-3;j++){
+       if(j>i+1 && nums[j-1]==nums[j]){
+        continue;
+       }
+       int l=j+1,m=nums.size()-1;
+       while(l<m){
+        long long sum=(long long)nums[i]+nums[j]+nums[l]+nums[m];
+        
+        if(sum==target){
+            ans.push_back({nums[i],nums[j],nums[l],nums[m]});
+            while(l<m && nums[l+1]==nums[l]) l++;
+            while(l<m && nums[m-1]==nums[m]) m--;
+            l++;
+            m--;
+        }else if(sum<target)
+        l++;
+        else
+        m--;
+       }
     }
-    return ans;
+
+   }
+   return ans;
     }
 };
