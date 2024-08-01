057494e17dd88cc403fcbeacba945cc0afc3e731
Akansha452001 <149137458+Akansha452001@users.noreply.github.com>
2024-08-01 22:21:55 +0530

    Time: 4 ms (47.1%), Space: 10.1 MB (13.22%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+ bool check(const string& length, const string& str) {
+        int n = length.length();
+        if (str.length() % n != 0) return false;
+
+        for (int i = 0; i < str.length(); i += n) {
+            if (str.substr(i, n) != length) return false;
+        }
+        return true;
+    }
+
+    // Function to find the greatest common divisor string of `str1` and `str2`
+    string gcdOfStrings(string str1, string str2) {
+        string ans;
+        int minLength = min(str1.length(), str2.length());
+        
+        for (int i = 1; i <= minLength; i++) {
+            // Only consider substrings that are possible divisors of both strings
+            if (str1.length() % i == 0 && str2.length() % i == 0) {
+                string length = str1.substr(0, i);
+
+                // Check if `length` can construct both `str1` and `str2`
+                if (check(length, str1) && check(length, str2)) {
+                    ans = length; // Update the answer with the current valid divisor
+                }
+            }
+        }
+        return ans;
+    }
+
+};
