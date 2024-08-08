ce9ec06644363d99134325e3ac74ade83f55dd75
Akansha452001 <149137458+Akansha452001@users.noreply.github.com>
2024-08-08 09:23:45 +0530

    Time: 7 ms (19.96%), Space: 12.3 MB (35.54%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,44 +1,46 @@
+#include <unordered_map>
+#include <string>
+
 class Solution {
 public:
-    string numberToWords(int num) {
-        if (num == 0) return "Zero";
-        
-        string bigString[] = {"Thousand", "Million", "Billion"};
-        string result = numberToWordsHelper(num % 1000);
-        num /= 1000;
+    std::unordered_map<int, std::string> below_ten = {
+        {0, ""}, {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
+    };
 
-        for (int i = 0; i < 3; ++i) {
-            if (num > 0 && num % 1000 > 0) {
-                result = numberToWordsHelper(num % 1000) + bigString[i] + " " + result;
-            }
-            num /= 1000;
+    std::unordered_map<int, std::string> below_twenty = {
+        {10, "Ten"}, {11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"}, {14, "Fourteen"},
+        {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"}
+    };
+
+    std::unordered_map<int, std::string> below_hundred = {
+        {2, "Twenty"}, {3, "Thirty"}, {4, "Forty"}, {5, "Fifty"},
+        {6, "Sixty"}, {7, "Seventy"}, {8, "Eighty"}, {9, "Ninety"}
+    };
+
+    std::string solve(int num) {
+        if (num < 10) {
+            return below_ten[num];
+        } else if (num < 20) {
+            return below_twenty[num];
+        } else if (num < 100) {
+            return below_hundred[num / 10] + ((num % 10 != 0) ? " " + below_ten[num % 10] : "");
+        } else if (num < 1000) {
+            return below_ten[num / 100] + " Hundred" + ((num % 100 != 0) ? " " + solve(num % 100) : "");
+        } else if (num < 1000000) {
+            return solve(num / 1000) + " Thousand" + ((num % 1000 != 0) ? " " + solve(num % 1000) : "");
+        } else if (num < 1000000000) {
+            return solve(num / 1000000) + " Million" + ((num % 1000000 != 0) ? " " + solve(num % 1000000) : "");
+        } else {
+            return solve(num / 1000000000) + " Billion" + ((num % 1000000000 != 0) ? " " + solve(num % 1000000000) : "");
         }
-
-        return result.empty() ? result : result.substr(0, result.size() - 1); // Trim trailing space
     }
 
-    string numberToWordsHelper(int num) {
-        string digitString[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-        string teenString[] = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        string tenString[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    std::string numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
 
-        string result = "";
-        if (num > 99) {
-            result += digitString[num / 100] + " Hundred ";
-        }
-        num %= 100;
-        if (num < 20 && num > 9) {
-            result += teenString[num - 10] + " ";
-        } else {
-            if (num >= 20) {
-                result += tenString[num / 10] + " ";
-            }
-            num %= 10;
-            if (num > 0) {
-                result += digitString[num] + " ";
-            }
-        }
-        
-        return result;
+        return solve(num);
     }
 };
